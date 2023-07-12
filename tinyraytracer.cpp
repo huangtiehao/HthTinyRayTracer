052bba959944feb92cb168c86eda2d39ab98a33e
huangtiehao <1634457608@qq.com>
2023-07-12 13:51:25 +0800

    step3

--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -28,7 +28,7 @@ public:
         this->radius = radius;
         this->material = material;
     }
-    bool raySphere_intersect(const Vec3f& lightPos,const Vec3f& lightDir)
+    bool raySphere_intersect(const Vec3f& lightPos,const Vec3f& lightDir,float& dist)
     {
         Vec3f light=lightDir - lightPos;
         Vec3f pc = center - lightPos;
@@ -38,10 +38,17 @@ public:
             return false;
         }
         float pdLen = pc.norm() * cosTheta;
-        float dcLen = (pc.norm()*pc.norm() - pdLen * pdLen);
-        if (dcLen <= 0)return true;
+        float dcLen = (pc.norm() * pc.norm() - pdLen * pdLen);
         dcLen = sqrt(dcLen);
-        if (dcLen <= radius)return true;
+        if (dcLen <= radius)
+        {
+            if (pdLen - sqrt(radius * radius - dcLen * dcLen) < dist)
+            {
+                dist = pdLen - sqrt(radius * radius - dcLen);
+                return true;
+            }
+            else return false;
+        }
         return false;
     }
 };
@@ -71,16 +78,17 @@ void render(std::vector<Sphere>& spheres)
             Vec3f light_position(0, 0, 0);
             Vec3f light_dir(x, y, -1);
             int sz = spheres.size();
+            float dist=10000000;
             for (int k = 0; k < sz; ++k)
             {
-                if (spheres[k].raySphere_intersect(light_position, light_dir))
+                if (spheres[k].raySphere_intersect(light_position, light_dir,dist))
                 {
                     framebuffer[j * width + i] = spheres[k].material.diffuse_color;
                 }
-                else
-                {
-                    framebuffer[j * width + i] = Vec3f(0.2,0.7,0.8);
-                }
+            }
+            if (dist == 10000000)
+            {
+                framebuffer[j * width + i] = Vec3f(0.2,0.7,0.8);
             }
         }
     }
@@ -92,10 +100,10 @@ int main() {
     Material red_rubber(Vec3f(0.3, 0.1, 0.1));
 
     std::vector<Sphere> spheres;
-    spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
     spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
     spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber));
     spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, ivory));
+    spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
 
     render(spheres);
     return 0;
